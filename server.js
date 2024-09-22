e11af734c8e7e35fc40f1b6f793fce5f3b91c63a
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-09-22 14:01:20 -0400

    work plz

--- server.js
+++ server.js
@@ -158,5 +158,5 @@ function checkNotAuthenticated(req, res, next) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
