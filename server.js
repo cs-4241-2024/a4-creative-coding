cba5ae149f2c57089f3471cea005608bb0a4a10c
twinz300 <wrlewis@wpi.edu>
2024-09-23 15:51:45 -0400

    work on glitch?

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(cors())
 app.use( express.static(dir)); //Allows "public" files to be accessed and utilized.
 
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/public/index.html')
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname + '/public/index.html')
 
 
-})
+// })
 
 app.listen (process.env.PORT || 3000);
