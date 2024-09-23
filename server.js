4e101fd4321a7e75d2a3c6a1198e42ecc02aeee5
twinz300 <wrlewis@wpi.edu>
2024-09-23 16:17:06 -0400

    47

--- server.js
+++ server.js
@@ -3,22 +3,18 @@
 const express = require('express');
 const cors = require('cors');
 const dir  = 'public/';
-const path = require('path') //new line for submission
 const port = 3000;
 
 const app = express();
 
 app.use(cors())
-// app.use( express.static(dir)); //Allows "public" files to be accessed and utilized.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( express.static(dir)); //Allows "public" files to be accessed and utilized.
+
 
 app.get('/', (req, res)=>{
-         res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(__dirname + '/index.html') //(__dirname + '/public/index.html')
+
+
 })
-// app.get('/', (req, res)=>{
-//     res.sendFile(__dirname + '/public/index.html')
-
-
-// })
 
 app.listen (process.env.PORT || 3000);
