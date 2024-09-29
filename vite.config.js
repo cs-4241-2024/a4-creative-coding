39abfede9b4d43d36bfc81d912c81d92077e30e7
HannaTrinh <trinhmaianhacg@gmail.com>
2024-09-29 14:48:53 -0400

    Fix vercel config for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
-        outDir: 'dist',  // Output to "dist" inside the project root
-        emptyOutDir: true,  // Clear the output directory before building
+        outDir: 'dist',  // Output folder inside the project root
+        emptyOutDir: true,  // Clear output before building
         rollupOptions: {
-            input: '/public/index.html',  // Input file is the public/index.html
+            input: './public/index.html',  // Ensure correct input file for the build
         },
     },
 });
